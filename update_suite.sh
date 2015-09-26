b8c26f713d29385acd35040ec276e123d8e9b811
Walter Schulze <awalterschulze@gmail.com>
2015-09-26 14:45:22 +0200

    added test suite

---
+++ update_suite.sh
@@ -0,0 +1,3 @@
+set -xe
+#git subtree add --prefix=JSON-Schema-Test-Suite https://github.com/json-schema/JSON-Schema-Test-Suite master --squash
+git subtree pull --prefix=JSON-Schema-Test-Suite https://github.com/json-schema/JSON-Schema-Test-Suite master --squash
