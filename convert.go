bd37daa45b2186404a500abce0934c4eba2f610b
Walter Schulze <awalterschulze@gmail.com>
2015-09-26 14:43:20 +0200

    first commit

---
+++ convert.go
@@ -0,0 +1,9 @@
+package jsonschema
+
+import (
+	"github.com/katydid/katydid/relapse/ast"
+)
+
+func Convert() *relapse.Grammar {
+	return nil
+}
---
+++ schema_test.go
@@ -0,0 +1,9 @@
+package jsonschema
+
+import (
+	"testing"
+)
+
+func TestDraft4(t *testing.T) {
+
+}
